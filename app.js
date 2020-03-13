b79b0fdb0c1a3ef76b42e38daa7fbc90c7ee2b14
bilalbrgt <47894927+bilalbrgt@users.noreply.github.com>
2020-03-13 23:37:37 +0100

    photos

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+require("sqreen");
+
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
+
 const app = express();
 
 var cors = require("cors");
 const morgan = require("morgan");
 app.use(cors());
 app.use(morgan("dev"));
+app.use(helmet());
+
 const route = require("./src/routes");
 
 app.use(express.static("assets"));
@@ -19,5 +25,4 @@ app.listen(8080, function() {
   console.log("listen on port 8080");
 
   // async..await is not allowed in global scope, must use a wrapper
-
 });
--- src/database/seeds/202003031513-travelagency.js
+++ src/database/seeds/202003031513-travelagency.js
@@ -8,11 +8,44 @@ const now = moment()
 module.exports = [
   {
     id: uuid(),
-    titre: "voyage en belgique",
-    lieu: "venise",
+    titre: "Voyages en Polynesie",
+    lieu: "polynesie",
     image:
-      "https://firebasestorage.googleapis.com/v0/b/travelagency-e6d12.appspot.com/o/ribblehead-viaduct-2443085_640.jpg?alt=media&token=8116db5c-027a-47a3-9be8-3a3d09578cd3",
-    prix: 400,
+      "https://firebasestorage.googleapis.com/v0/b/travelagency-e6d12.appspot.com/o/background-2413081_1280.jpg?alt=media&token=2f8821e7-2bde-400f-b475-0b138b61d144",
+    prix: 250,
+    descriptions: " ",
+    created_at: now,
+    updated_at: now
+  },
+  {
+    id: uuid(),
+    titre: "  Mexique ",
+    lieu: "",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/travelagency-e6d12.appspot.com/o/quentin-dang-GF5SR2ETysg-unsplash.jpg?alt=media&token=b9c8167b-1b9f-4439-bc5f-223b3da7885c",
+    prix: 250,
+    descriptions: "",
+    created_at: now,
+    updated_at: now
+  },
+  {
+    id: uuid(),
+    titre: "thaiti",
+    lieu: "",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/travelagency-e6d12.appspot.com/o/sara-dubler-Koei_7yYtIo-unsplash-compressor.jpg?alt=media&token=eda834b3-23cf-484e-81c1-a2459d462c7a",
+    prix: 150,
+    descriptions: "  ",
+    created_at: now,
+    updated_at: now
+  },
+  {
+    id: uuid(),
+    titre: "mexique",
+    lieu: "",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/travelagency-e6d12.appspot.com/o/harry-cunningham-N4bOFvyrXgo-unsplash-compressor.jpg?alt=media&token=cc13efff-f8a8-48fc-870e-4c0ff5a9df16",
+    prix: 50,
     descriptions: " forza italie",
     created_at: now,
     updated_at: now
